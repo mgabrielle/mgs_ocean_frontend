a5c96b272f21ae9f6ad5f223c26c9c33be50329c
mgabrielle <60402418+mgabrielle@users.noreply.github.com>
2023-09-27 22:56:52 +0000

    Atualizando itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    nome: 'Summer Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <div className='card'>
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
     </>
   )
 }
